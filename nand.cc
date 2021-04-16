b8889289862b866062d45a9087920219aac5e76b
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-04-16 23:11:04 +0900

    merge externals, output change.

--- nand.cc
+++ nand.cc
@@ -3,58 +3,13 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <map>
 #include <iomanip>
 #include <algorithm>
 #include <assert.h>
 
-#if !defined(_FLOAT_BITS_)
-  #include <complex>
-  #include <cmath>
-  using namespace std;
-  typedef long double num_t;
-#else
-  #include "ifloat.hh"
-  template <typename T> using complex = Complex<T>;
-# if _FLOAT_BITS_ == 8
-    typedef uint8_t myuint;
-    typedef int8_t  myint;
-    typedef SimpleFloat<myuint, uint16_t, 8, int64_t> num_t;
-    #define mybits 8
-# elif _FLOAT_BITS_ == 16
-    typedef uint16_t myuint;
-    typedef int16_t  myint;
-    typedef SimpleFloat<myuint, uint32_t, 16, int64_t> num_t;
-    #define mybits 16
-# elif _FLOAT_BITS_ == 32
-    typedef uint32_t myuint;
-    typedef int32_t  myint;
-    typedef SimpleFloat<myuint, uint64_t, 32, int64_t> num_t;
-    #define mybits 32
-# elif _FLOAT_BITS_ == 64
-    typedef uint64_t myuint;
-    typedef int64_t  myint;
-    typedef SimpleFloat<myuint, DUInt<myuint, 64>, 64, int64_t> num_t;
-    #define mybits 64
-# elif _FLOAT_BITS_ == 128
-    typedef DUInt<uint64_t, 64> uint128_t;
-    typedef Signed<uint128_t, 128> int128_t;
-    typedef uint128_t myuint;
-    typedef int128_t  myint;
-    typedef SimpleFloat<myuint, DUInt<myuint, 128>, 128, int64_t> num_t;
-    #define mybits 128
-# elif _FLOAT_BITS_ == 256
-    typedef DUInt<uint64_t, 64> uint128_t;
-    typedef DUInt<uint128_t, 128> uint256_t;
-    typedef Signed<uint256_t, 128> int256_t;
-    typedef uint256_t myuint;
-    typedef int256_t  myint;
-    typedef SimpleFloat<myuint, DUInt<myuint, 256>, 256, int64_t> num_t;
-    #define mybits 256
-# else
-#   error cannot handle float
-# endif
-#endif
-
+#include "ifloat.hh"
+typedef myfloat num_t;
 #include "simplelin.hh"
 
 int main(int argc, char* argv[])
@@ -76,8 +31,6 @@ int main(int argc, char* argv[])
     A.row(j) *= r;
     A(j, 3)   = num_t(1);
   }
-  for(int j = 0; j < one.size(); j ++)
-    std::cout << A(j, 0) << ", " << A(j, 1) << ", " << A(j, 2) << ", " << A(j, 3) << std::endl;
   const auto in(A.inner(one, one));
   const auto err(A * in);
         auto M(abs(err[0]));
@@ -86,7 +39,7 @@ int main(int argc, char* argv[])
     M = max(M, abs(err[j]));
     m = min(m, abs(err[j]));
   }
-  std::cout << m << ", " << M << ", " << sqrt(in.dot(in)) << std::endl;
+  std::cout << A << in << std::endl;
   // result of this M, m, ||in|| limit seems to 1.
   return 0;
 }
