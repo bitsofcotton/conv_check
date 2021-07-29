1c7bba2a9b80f2a32e61a50749b7e78900be297b
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-07-29 14:27:36 +0900

    nand fix.

--- nand.cc
+++ nand.cc
@@ -18,10 +18,10 @@ typedef myfloat num_t;
 #include <cmath>
 
 int main(int argc, char* argv[]) {
-  SimpleMatrix<num_t> A(9, 5);
+  SimpleMatrix<num_t> A(8 + 1, 6);
   SimpleVector<num_t> left(A.rows());
   SimpleVector<num_t> right(A.rows());
-  for(int j = 0; j < 4; j ++) {
+  for(int j = 0; j < 8; j ++) {
     SimpleVector<num_t> work(4);
     work[0]  = num_t(j & 1 ? 1 : 2) / num_t(2);
     work[1]  = num_t((j >> 1) & 1 ? 1 : 2) / num_t(2);
@@ -31,12 +31,10 @@ int main(int argc, char* argv[]) {
     assert(A.cols() == A.row(j).size());
     left[j]  = - (right[j] = sqrt(A.epsilon));
   }
-  for(int i = 4; i < A.rows(); i ++) {
-    for(int j = 0; j < A.cols(); j ++)
-      A(i, j) = num_t(i - 4 == j ? 1 : 0);
-    left[i]  = sqrt(sqrt(A.epsilon));
-    right[i] = num_t(1) / sqrt(sqrt(A.epsilon));
-  }
+  for(int j = 0; j < A.cols(); j ++)
+    A(8, j) = num_t(4 == j ? 1 : 0);
+  left[8]  = sqrt(sqrt(A.epsilon));
+  right[8] = num_t(1) / sqrt(sqrt(A.epsilon));
   const auto in(A.inner(left, right));
   std::cout << A * in << std::endl;
   return 0;
