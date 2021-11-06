b2024701000db4f4c6be3792ac65e6842a1cc600
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-11-06 19:39:39 +0900

    don't know why but there's unintended revert, fix

--- svd.cc
+++ svd.cc
@@ -8,18 +8,15 @@
 #include "lieonn.hh"
 
 int main(int argc, char* argv[]) {
-  SimpleMatrix<myfloat> A(12, 8);
+  assert(2 < argc);
+  SimpleMatrix<myfloat> A(std::atoi(argv[1]), std::atoi(argv[2]));
   for(int i = 0; i < A.rows(); i ++)
     for(int j = 0; j < A.cols(); j ++)
-      A(i, j) = myfloat(int(arc4random())) / myfloat(int(arc4random()));
-  const auto svd(A.SVD());
-        auto rsvd(svd * A);
-  for(int i = 0; i < rsvd.rows(); i ++)
-    rsvd.row(i) /= sqrt(rsvd.row(i).dot(rsvd.row(i)));
-  std::cout <<  svd *  svd.transpose();
-  std::cout << rsvd * rsvd.transpose();
-  std::cout <<  svd.transpose() * svd;
-  std::cout << rsvd.transpose() * rsvd;
+      A(i, j) = myfloat(int(arc4random()));
+  auto left(A.SVD() * A);
+  for(int i = 0; i < left.rows(); i ++)
+    left.row(i) /= sqrt(left.row(i).dot(left.row(i)));
+  std::cout << left * left.transpose();
   return 0;
 }
 
